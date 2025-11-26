1820130ede9b851f26007b49033fe26bd6d87b79
crstofrd <christogumerung@gmail.com>
2025-11-26 13:02:07 +0800

    Menambahkan prgram input handler yang telah dibuat oleh anggota 3 ke main
    Add function to get user input for numbers with validation

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+def get_numbers():
+    while True:
+        try:
+            a = float(input("Masukkan angka pertama: "))
+            b = float(input("Masukkan angka kedua: "))
+            return a, b
+        except ValueError:
+            print("Angka tidak valid! Masukkan angka dengan benar.")
+
 def tambah(a, b):
     return a + b
 
