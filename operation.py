8460b6219574f09cf91c5f5b6a702dc3931a1752
crstofrd <christogumerung@gmail.com>
2025-11-26 13:05:12 +0800

    Delete operation.py untuk merapikan repo

--- operation.py
+++
@@ -1,8 +0,0 @@
-#operation.py 
-def tambah(a,b): return a + b
-def kurang(a,b): return a + b 
-def kali(a,b):return a * b 
-def bagi(a,b): if b==0:
-return "Error: Tidak bisa membagi dengan nol1"
-return a/b
-
